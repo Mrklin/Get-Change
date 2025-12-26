4d2b3511def2051eff403c7391ea60bcc730ac88
Mrklin <mrklin244@gmail.com>
2025-12-26 11:51:26 +0100

    Adding batch 4

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script >
+import AuthLayoutView from './pages/AuthLayoutView.vue';
+export default {
+  components: {
+    AuthLayoutView,
+  },
+}
+
+</script>
+
+<template>
+  
+  <!-- <AuthLayoutView /> -->
+
+  <RouterView />
+</template>
+
+
